913b09807361c5e0e64aa9b4470c23ad6d68e04d
Juan <juan.e.gregorio@gmail.com>
2021-05-13 20:57:46 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container-sm">
+    <h1>Employees</h1>
+    <employee-form @add:employee="addEmployee" />
+    <!-- add @delete -->
+    <employee-table :employees="employees"
+    @delete:employee="deleteEmployee" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import EmployeeTable from '@/components/EmployeeTable.vue'
+  import EmployeeForm from '@/components/EmployeeForm.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      EmployeeTable,
+      EmployeeForm,
+    },
+    data() {
+      return {
+        employees: [
+          {
+            id: 1,
+            name: 'Richard Hendricks',
+            email: 'richard@piedpiper.com',
+          },
+          {
+            id: 2,
+            name: 'Bertram Gilfoyle',
+            email: 'gilfoyle@piedpiper.com',
+          },
+          {
+            id: 3,
+            name: 'Dinesh Chugtai',
+            email: 'dinesh@piedpiper.com',
+          },
+        ],
+      }
+    },
+    methods: {
+      addEmployee(employee) {
+        // this.employees = [...this.employees, employee]
+        // this one stuck, it does not move to the next one
+        const lastId =
+          this.employees.length > 0
+            ? this.employees[this.employees.length - 1].id
+            : 0;
+        const id = lastId + 1;
+        const newEmployee = { ...employee, id };
+        this.employees = [...this.employees, newEmployee];
+        // this one, you can keep add more employees
+      },
+      // add delete employee
+      deleteEmployee(id) {
+        this.employees = this.employees.filter(employee => employee.id !== id)
+      },
+    },
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  button {
+  background: #009435;
+    border: 1px solid #009435;
+  }
+  .container-sm {
+    max-width: 680px;
+  } 
 </style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="employee-table">
+    <p v-if="employees.length < 1" class="empty-table">No employees</p>
+    <table v-else class="table table-striped">
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+          <td>
+            <button>Edit</button>
+            <!-- add delete button -->
+            <button @click="$emit('delete:employee', employee.id)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EmployeeTable',
+    props: {
+      employees: Array,
+    },
+  }
+</script>
+
+<style scoped></style>
